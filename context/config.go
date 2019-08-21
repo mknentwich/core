2927d4f9f9b32fdc48348745b3885f43dae78b1a
Richard Stöckl <richard.stoeckl@aon.at>
2019-08-21 12:35:36 +0200

    added configuration

---
+++ context/config.go
@@ -0,0 +1,56 @@
+package context
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"os"
+)
+
+const configFile = "config.json"
+
+//Struct for the configuration of the application.
+type Configuration struct {
+	Host                 string `json:"host"`
+	JWTExpirationMinutes int    `json:"jwtExpirationMinutes"`
+	JWTSecret            string `json:"jwtSecret"`
+	SQLiteFile           string `json:"sqliteFile"`
+}
+
+//Returns a default configuration with a generated jwt secret.
+func defaultConf() *Configuration {
+	secret := make([]byte, 16)
+	rand.Read(secret)
+	return &Configuration{
+		Host:                 "0.0.0.0:9400",
+		JWTExpirationMinutes: 5,
+		JWTSecret:            fmt.Sprintf("%x", secret),
+		SQLiteFile:           "core.sqlite"}
+}
+
+//Reads configuration from file and creates one if it does not exist yet.
+func config() (*Configuration, error) {
+	conf, err := os.Open(configFile)
+	if err != nil {
+		conf, err = os.Create(configFile)
+		if err != nil {
+			return nil, err
+		}
+		encoder := json.NewEncoder(conf)
+		encoder.SetIndent("", "  ")
+		configuration := defaultConf()
+		err = encoder.Encode(configuration)
+		if err != nil {
+			return configuration, err
+		}
+		return configuration, conf.Close()
+	}
+	decoder := json.NewDecoder(conf)
+	configuration := &Configuration{}
+	err = decoder.Decode(configuration)
+	return configuration, err
+	if err != nil {
+		return configuration, err
+	}
+	return configuration, conf.Close()
+}
---
+++ context/config_test.go
@@ -0,0 +1,47 @@
+package context
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+)
+
+//Fails a test if an unexpected error occur which has nothing to do with the test case.
+func unexpected(t *testing.T, err error) {
+	if err != nil {
+		t.Fatalf("Encountered unexpected error: %s", err.Error())
+	}
+}
+
+//Removes config file before and after every test case.
+func TestMain(m *testing.M) {
+	os.Remove(configFile)
+	code := m.Run()
+	os.Remove(configFile)
+	os.Exit(code)
+}
+
+//Tests if a config will we generated when no exists.
+func TestConfigCreation(t *testing.T) {
+	_, err := config()
+	if err != nil {
+		t.Errorf("Error on creating a config file: %s", err.Error())
+	}
+}
+
+//Tests if the read config is the persisted one.
+func TestConfigRead(t *testing.T) {
+	custom := &Configuration{Host: "73.73.73.73:80", JWTExpirationMinutes: 1, SQLiteFile: "db.sqlite"}
+	file, err := os.Create(configFile)
+	unexpected(t, err)
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(custom)
+	unexpected(t, err)
+	err = file.Close()
+	unexpected(t, err)
+	read, err := config()
+	unexpected(t, err)
+	if read.Host != custom.Host || read.JWTExpirationMinutes != custom.JWTExpirationMinutes || read.SQLiteFile != custom.SQLiteFile {
+		t.Errorf("Expected configuration: %v, but got: %v", *custom, *read)
+	}
+}
