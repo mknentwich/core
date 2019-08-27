03d4855fa88fb8d1618015cd8469392e99411e6c
Richard Stöckl <richard.stoeckl@aon.at>
2019-08-27 15:47:58 +0200

    disabled auto_preload

--- database/initialize.go
+++ database/initialize.go
@@ -18,7 +18,7 @@ func initializeDb() error {
 	if err != nil {
 		return err
 	}
-	database = db.Exec("pragma foreign_keys = on;").AutoMigrate(&Address{}, &Category{}, &Order{}, &Score{}, &User{}).Set("gorm:auto_preload", true)
+	database = db.Exec("pragma foreign_keys = on;").AutoMigrate(&Address{}, &Category{}, &Order{}, &Score{}, &User{})
 	return err
 }
 
