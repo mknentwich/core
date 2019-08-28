da3b5da5741549dcb65c93c6b89ceacfc6dcd717
Richard Stöckl <richard.stoeckl@aon.at>
2019-08-28 11:46:26 +0200

    only query necessary categories

--- rest/query.go
+++ rest/query.go
@@ -10,9 +10,9 @@ type DataQuery func() interface{}
 func QueryCategoriesWithChildrenAndScores() interface{} {
 	categories := make([]database.Category, 0)
 	database.Receive().Preload("Children", func(d *gorm.DB) *gorm.DB {
-		return d.Order("name").Preload("Scores", func(d2 *gorm.DB) *gorm.DB {
+		return d.Joins("inner join scores s on categories.id = s.category_id").Order("name").Preload("Scores", func(d2 *gorm.DB) *gorm.DB {
 			return d2.Order("title")
 		})
-	}).Where("parent_id is null").Order("name").Find(&categories)
+	}).Where("categories.parent_id is null").Joins("inner join categories c on c.parent_id = categories.id").Joins("inner join scores s on s.category_id = c.id").Order("name").Find(&categories)
 	return categories
 }
