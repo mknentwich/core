ac32f758eb8155881f03548514c82e56200c2f86
Dennis Toth <dennis.toth@gmx.at>
2019-09-09 14:49:40 +0200

    added insert db functions, query for pdf bill and tests

---
+++ rest/query_test.go
@@ -0,0 +1,92 @@
+package rest
+
+import (
+	"github.com/mknentwich/core/context"
+	"github.com/mknentwich/core/database"
+	"os"
+	"reflect"
+	"testing"
+)
+
+//Starts database service at begin of testing
+func TestMain(m *testing.M) {
+	context.InitializeCustomConfig(map[string]context.Serve{
+		"/db": database.Serve,
+	},
+		&context.Configuration{
+			Host:       "0.0.0.0:9400",
+			SQLiteFile: ":memory:",
+		})
+	os.Exit(m.Run())
+}
+
+//Tests inserts of all tables
+func TestPDFOrderResult(t *testing.T) {
+	address := database.Address{
+		City:         "Hürth",
+		PostCode:     "50354",
+		State:        "DEUTSCHLAND",
+		Street:       "Kalscheurener Straße",
+		StreetNumber: "89",
+	}
+	err := InsertNewAddress(address)
+	if err != nil {
+		t.Errorf("Error on inserting at the addresses table: %s", err.Error())
+	}
+	category := database.Category{
+		Name: "Polka",
+	}
+	err = InsertNewCategory(category)
+	if err != nil {
+		t.Errorf("Error on inserting at the categories table: %s", err.Error())
+	}
+	score := database.Score{
+		CategoryID: 1,
+		Difficulty: 3,
+		Price:      39.9,
+		Title:      "Eine letzte Runde (Blasorchesterfassung)",
+	}
+	err = InsertNewScore(score)
+	if err != nil {
+		t.Errorf("Error on inserting at the scores table: %s", err.Error())
+	}
+	order := database.Order{
+		BillingAddressID:  1,
+		Company:           "Millionen",
+		Date:              1568024628000,
+		DeliveryAddressID: 1,
+		Email:             "jauch@werwirdswohl.de",
+		FirstName:         "Günter",
+		LastName:          "Jauch",
+		Payed:             false,
+		ReferenceCount:    0,
+		Salutation:        "Herr",
+		ScoreID:           1,
+		ScoreAmount:       1,
+		Telephone:         "",
+	}
+	err = InsertNewOrder(order)
+	if err != nil {
+		t.Errorf("Error on inserting at the orders table: %s", err.Error())
+	}
+	result := QueryOrderFromIdForPDF(1)
+	expectedResult := PDFOrderResult{
+		City:         "Hürth",
+		PostCode:     "50354",
+		State:        "DEUTSCHLAND",
+		Street:       "Kalscheurener Straße",
+		StreetNumber: "89",
+		ID:           1,
+		Company:      "Millionen",
+		Date:         1568024628000,
+		FirstName:    "Günter",
+		LastName:     "Jauch",
+		Salutation:   "Herr",
+		ScoreAmount:  1,
+		Title:        "Eine letzte Runde (Blasorchesterfassung)",
+		Price:        39.9,
+	}
+	if reflect.DeepEqual(result, expectedResult) == false {
+		t.Errorf("%s", "Object from the database and local Object aren't the same!")
+	}
+}
