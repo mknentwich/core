e5b2c5ecd07a85278f8d7f77c06e05f04846158c
Richard Stöckl <richard.stoeckl@aon.at>
2019-09-09 20:55:44 +0200

    added some auth tests

--- auth/auth_test.go
+++ auth/auth_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/mknentwich/core/context"
 	"github.com/mknentwich/core/database"
 	"github.com/mknentwich/core/utils"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"testing"
@@ -24,38 +25,97 @@ func TestMain(m *testing.M) {
 	os.Exit(code)
 }
 
-func call(str string) string {
+//Appends Host URL.
+func canonical(str string) string {
 	return context.Conf.Host + "/auth" + str
 }
 
+//Compares both HTTP status and let the test fail if necessary.
+func checkHttpStatus(expected int, status int, t *testing.T) {
+	if expected != status {
+		t.Errorf("HTTP status code should be %d but was %d!", expected, status)
+	}
+}
+
+//Compares a credentials email with a users once and let the test fail if necessary.
+func checkUserInfo(expected credentials, actual database.User, t *testing.T) {
+	if expected.email != actual.Email {
+		t.Errorf("Server returned wrong user. Expected email: %s, got %s", expected.email, actual.Email)
+	}
+}
+
+//Sets the header for REST requests.
 func header(r *http.Request) {
 	r.Header.Set("Content-Type", "application/json")
 }
 
-func post(body interface{}, t *testing.T) *http.Response {
-	data, err := json.Marshal(body)
+//Tries to login and returns it response.
+func login(credentials credentials, t *testing.T) *http.Response {
+	data, err := json.Marshal(credentials)
 	utils.Unexpected(t, err)
-	request, err := http.NewRequest(http.MethodPost, call("/login"), bytes.NewBuffer(data))
+	request, err := http.NewRequest(http.MethodPost, canonical("/login"), bytes.NewBuffer(data))
 	header(request)
 	response, err := http.DefaultClient.Do(request)
 	utils.Unexpected(t, err)
 	return response
 }
 
+//Renews a given JWT and returns the new one and the HTTP status.
+func renew(jwt string, t *testing.T) (string, int) {
+	r, err := http.NewRequest(http.MethodGet, jwt, nil)
+	r.Header.Set("Authorization: Bearer ", jwt)
+	utils.Unexpected(t, err)
+	response, err := http.DefaultClient.Do(r)
+	utils.Unexpected(t, err)
+	body, err := ioutil.ReadAll(response.Body)
+	utils.Unexpected(t, err)
+	return string(body), response.StatusCode
+}
+
+//Fetches a User from a JWT. Returns the HTTP status too.
+func userInfo(jwt string, t *testing.T) (database.User, int) {
+	r, err := http.NewRequest(http.MethodGet, canonical("/self"), nil)
+	r.Header.Set("Authorization", "Bearer "+jwt)
+	utils.Unexpected(t, err)
+	response, err := http.DefaultClient.Do(r)
+	utils.Unexpected(t, err)
+	user := database.User{}
+	json.NewDecoder(response.Body).Decode(&user)
+	return user, response.StatusCode
+}
+
+//Tests a wrong password on `/login`
 func TestInvalidCredentials(t *testing.T) {
 	cr := albert
 	cr.password += "sdklfgdklfj"
-	response := post(cr, t)
-	status := http.StatusForbidden
-	if response.StatusCode != status {
-		t.Errorf("HTTP status code should be %d but was %d!", status, response.StatusCode)
-	}
+	response := login(cr, t)
+	checkHttpStatus(http.StatusForbidden, response.StatusCode, t)
 }
 
-func TestInvalidRenewal(t *testing.T) {}
+//Tests an invalid JWT format
+func TestInvalidJWTFormat(t *testing.T) {
+	_, status := userInfo("skdjfhskdjfhksjhrjkrg", t)
+	checkHttpStatus(http.StatusUnprocessableEntity, status, t)
+}
 
-func TestValidRenewal(t *testing.T) {}
+//Tests a JWT renewal with a valid JWT
+func TestValidRenewal(t *testing.T) {
+	response := login(albert, t)
+	jwt, err := ioutil.ReadAll(response.Body)
+	utils.Unexpected(t, err)
+	renewJwt, status := renew(string(jwt), t)
+	checkHttpStatus(http.StatusOK, status, t)
+	user, status := userInfo(renewJwt, t)
+	checkHttpStatus(http.StatusOK, status, t)
+	checkUserInfo(albert, user, t)
+}
 
-func TestValidUserInfo(t *testing.T) {}
-
-func TestInvalidUserInfo(t *testing.T) {}
+//Tests the user info
+func TestUserInfo(t *testing.T) {
+	response := login(albert, t)
+	jwt, err := ioutil.ReadAll(response.Body)
+	utils.Unexpected(t, err)
+	user, status := userInfo(string(jwt), t)
+	checkHttpStatus(http.StatusOK, status, t)
+	checkUserInfo(albert, user, t)
+}
