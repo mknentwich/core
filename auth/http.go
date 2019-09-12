a359fa13de55febb006c63c0b4b163fef70151a3
Richard Stöckl <richard.stoeckl@aon.at>
2019-09-12 16:48:43 +0200

    added Auth and Admin handlerFunc wrappers

--- auth/http.go
+++ auth/http.go
@@ -93,3 +93,25 @@ func httpSelf(rw http.ResponseWriter, r *http.Request) {
 func getJwt(r *http.Request) string {
 	return strings.Replace(r.Header.Get("Authorization"), "Bearer ", "", 1)
 }
+
+//Wrapper for HandlerFuncs to verify if user is logged in and if admin is necessary or not.
+func authenticated(handlerFunc http.HandlerFunc, admin bool) http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		user, _, err := Check(getJwt(r))
+		if err == nil && user != nil && (user.Admin || !admin) {
+			handlerFunc(rw, r)
+		} else {
+			rw.WriteHeader(http.StatusForbidden)
+		}
+	}
+}
+
+//Checks if user is logged in. If not, the handlerFunc won't be called.
+func Auth(handlerFunc http.HandlerFunc) http.HandlerFunc {
+	return authenticated(handlerFunc, false)
+}
+
+//Checks if user is admin. If not, the handlerFunc won't be called.
+func Admin(handlerFunc http.HandlerFunc) http.HandlerFunc {
+	return authenticated(handlerFunc, true)
+}
