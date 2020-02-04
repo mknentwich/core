51fc9d9d2701747a4286cb8fbb0d67d92615df41
Richard Stöckl <richard.stoeckl@aon.at>
2020-02-04 10:37:50 +0100

    filled default config

--- context/config.go
+++ context/config.go
@@ -38,12 +38,27 @@ func defaultConf() *Configuration {
 	secret := make([]byte, 16)
 	rand.Read(secret)
 	return &Configuration{
-		Authentication:       true,
-		GeneratedDirectory:   "gen",
-		Host:                 "0.0.0.0:9400",
+		Authentication:     true,
+		GeneratedDirectory: "gen",
+		Host:               "0.0.0.0:9400",
+		Mail: EmailCredentials{
+			Username: "max",
+			Password: "thatsmaxmaildeliverypassword",
+			SMTPHost: "mail.example.org",
+			Address: &mail.Address{
+				Name:    "Max Mustermann",
+				Address: "noreply@mail.example.org",
+			},
+		},
 		JWTExpirationMinutes: 5,
 		JWTSecret:            fmt.Sprintf("%x", secret),
-		SQLiteFile:           "core.sqlite"}
+		SQLiteFile:           "core.sqlite",
+		TemplateInterval:     5,
+		OrderRetrievers: []*mail.Address{
+			{
+				Name:    "Max Mustermann",
+				Address: "max.mustermann@mail.example.org",
+			}}}
 }
 
 //Reads configuration from file and creates one if it does not exist yet.
