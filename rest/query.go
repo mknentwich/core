34e0a14f49d51af02934cae98740d0a0fb1d8f5f
Richard Stöckl <richard.stoeckl@aon.at>
2020-03-02 17:20:25 +0100

    added second join layer

--- rest/query.go
+++ rest/query.go
@@ -17,6 +17,8 @@ func QueryCategoriesWithChildrenAndScoresPreserve() []database.Category {
 		return d.Order("name").Preload("Scores", func(d2 *gorm.DB) *gorm.DB {
 			return d2.Order("title")
 		})
+	}).Preload("Scores", func(d2 *gorm.DB) *gorm.DB {
+		return d2.Order("title")
 	}).Where("categories.parent_id is null").Order("name").Find(&categories)
 	return categories
 }
